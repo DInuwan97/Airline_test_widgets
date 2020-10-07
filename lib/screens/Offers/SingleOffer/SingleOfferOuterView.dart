61600424fa12151be98323cb1ae4a846e6624905
DInuwan97 <kalubowila.dc25132@live.com>
2020-10-08 02:24:59 +0530

    Offers single page modified

--- lib/screens/Offers/SingleOffer/SingleOfferOuterView.dart
+++ lib/screens/Offers/SingleOffer/SingleOfferOuterView.dart
@@ -7,12 +7,41 @@ class SingleOfferOuterView extends StatelessWidget {
   final OfferedTrips offer;
   const SingleOfferOuterView({Key key, this. offer}) : super(key: key);
 
+  
+
 
   @override
   Widget build(BuildContext context) {
+     var appBar = AppBar(
+       title: Text(
+          offer.name,
+          style:TextStyle(
+            color:Colors.black
+          )
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+    );
+
+    final windowWidth = MediaQuery.of(context).size.width;
+    final windowHeight = MediaQuery.of(context).size.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    final appBarHeight = appBar.preferredSize.height;
+    final bodyHeight =
+        windowHeight - (statusBarHeight + appBarHeight + windowHeight * 0.079);
+    final faresHeight = bodyHeight -
+        (bodyHeight * 0.4 + 50 + windowHeight * 0.035 + windowHeight * 0.09);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(offer.name)
+        title: Text(
+          offer.name,
+          style:TextStyle(
+            color:Colors.white
+          )
+        ),
+        elevation: 0.0,
+        //backgroundColor: Colors.transparent,
       ),
 
       body:ListView(
@@ -96,15 +125,140 @@ class SingleOfferOuterView extends StatelessWidget {
                             ),
 
 
+                           
+
+
           ],
           ),
           
 
-      
+       travellingClassWidget(context)
           
         ],
       ),
       
     );
   }
+
+
+
+Widget travellingClassWidget(BuildContext context){
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical:8.0,horizontal:12.0),
+    child: Container(
+      height:MediaQuery.of(context).size.width * 0.3,
+      width:MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+
+              gradient: LinearGradient(
+                  colors: [Colors.blueAccent,Colors.blue],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+                borderRadius: BorderRadius.all(
+                    Radius.circular(20.0)
+                )
+              ),
+
+      child:Row(
+        children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical:25,horizontal:15),
+                  child: Column(
+                    children: <Widget>[
+                      RichText(
+                            text: TextSpan(
+                            text: 'ECONOMY ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize:20,
+                              fontFamily: 'Oxygen',
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'CLASS', 
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600
+                                )
+                              ),
+                            ],
+                          ),
+                      ),
+                    ],
+                  ),
+                ),
+
+
+                Spacer(),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical:15,horizontal:20),
+                  child: Column(
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {
+                         Navigator.push(context,
+                          MaterialPageRoute(
+    	                                            
+                          )
+                         );
+                        },
+                        padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.01,vertical: 8),
+                        
+                          child: const Text('BOOK NOW',
+                              style: TextStyle(
+                                fontSize: 15,
+                              )
+                          ),
+
+                          elevation: 10.0,
+                          color: Colors.blue[900],
+                          splashColor: Colors.blue[200],
+                          animationDuration:
+                              Duration(seconds: 2),
+                              colorBrightness: Brightness.dark,
+                              shape: RoundedRectangleBorder(
+                              borderRadius:
+                                new BorderRadius.circular(10.0),
+                            side: BorderSide(
+                                color: Colors.white,
+                                                                      
+                            ),
+                           
+                          )
+                          ),
+                    ],
+
+                  ),
+
+                  
+                ),
+
+                Column(
+                  children: <Widget>[
+                      //      RichText(
+                      //       text: TextSpan(
+                      //       text: 'USD 210',
+                      //       style: TextStyle(
+                      //         fontWeight: FontWeight.bold,
+                      //         fontSize:20,
+                      //         fontFamily: 'Oxygen',
+                      //       )
+                      
+                      //     ),
+                      // ),
+                  ]
+                )
+               
+
+        ],
+      )
+    ),
+  );
+}
+
+
+
 }
