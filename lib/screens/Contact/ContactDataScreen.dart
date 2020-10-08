fd046ca870c01bf6e998d88ebf276edd1f0da429
DInuwan97 <kalubowila.dc25132@live.com>
2020-10-08 10:45:10 +0530

    Single Offer screen modified

--- lib/screens/Contact/ContactDataScreen.dart
+++ lib/screens/Contact/ContactDataScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flag/flag.dart';
+//import 'package:flag/flag.dart';
 
 import 'Country1.dart';
 import 'Country2.dart';
