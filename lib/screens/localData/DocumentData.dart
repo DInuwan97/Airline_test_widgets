8d99abdce5cb701b507827ede194b63030dab780
Sachin Athukorala <sachin2262716@gmail.com>
2020-10-09 23:46:54 +0530

    format form codes

--- lib/screens/localData/DocumentData.dart
+++ lib/screens/localData/DocumentData.dart
@@ -10,8 +10,8 @@ class DocumentData extends StatefulWidget {
 }
 
 class _DocumentDataState extends State<DocumentData> {
-  final List<String> documentTypes = ['ID Card', 'Passport'];
-  final List<String> countries = [
+  final List<String> _documentTypes = ['ID Card', 'Passport'];
+  final List<String> _countries = [
     'Afghanistan',
     'Albania',
     'Algeria',
@@ -29,13 +29,43 @@ class _DocumentDataState extends State<DocumentData> {
     'Hong Kong',
     'Sri Lanka'
   ];
-  final List<String> nationalities = ['Sinhala', 'Tamil', 'Muslim'];
-  String selectedDocType = 'ID Card';
-  String selectedCountry = 'Sri Lanka';
-  String selectedNationality = 'Sinhala';
-  bool isBiometric = false;
+  final List<String> _nationalities = ['Sinhala', 'Tamil', 'Muslim'];
+  String _selectedDocType = 'ID Card';
+  String _selectedCountry = 'Sri Lanka';
+  String _selectedNationality = 'Sinhala';
+  bool _isBiometric = false;
   final _form = GlobalKey<FormState>();
   bool isLoading = false;
+
+  // form styles
+  final OutlineInputBorder _enabledBorder = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.white,
+      width: 1,
+    ),
+    borderRadius: BorderRadius.circular(20),
+  );
+  final OutlineInputBorder _focusedBorder = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.blue[300],
+      width: 2,
+    ),
+    borderRadius: BorderRadius.circular(20),
+  );
+  final OutlineInputBorder _errorBorder = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.red,
+      width: 2,
+    ),
+    borderRadius: BorderRadius.circular(20),
+  );
+  final OutlineInputBorder _focuseErrordBorder = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.red,
+      width: 2,
+    ),
+    borderRadius: BorderRadius.circular(20),
+  );
 
   void _submitForm() async {
     final bool isFormValid = _form.currentState.validate();
@@ -44,7 +74,7 @@ class _DocumentDataState extends State<DocumentData> {
     setState(() {
       isLoading = true;
     });
-    var timer = new Timer(const Duration(seconds: 2), () {
+    new Timer(const Duration(seconds: 2), () {
       _form.currentState.save();
       setState(() {
         isLoading = false;
@@ -73,12 +103,12 @@ class _DocumentDataState extends State<DocumentData> {
         key: _form,
         child: ListView(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 8,
             ),
             Container(
               margin: EdgeInsets.only(left: 22, bottom: 2),
-              child: Text(
+              child: const Text(
                 'DOCUMENT TYPE',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -95,11 +125,11 @@ class _DocumentDataState extends State<DocumentData> {
                   color: Colors.grey[800],
                   fontWeight: FontWeight.bold,
                 ),
-                value: selectedDocType,
+                value: _selectedDocType,
                 onChanged: (String value) =>
-                    setState(() => selectedDocType = value),
+                    setState(() => _selectedDocType = value),
                 selectedItemBuilder: (BuildContext context) {
-                  return documentTypes.map<Widget>((String t) {
+                  return _documentTypes.map<Widget>((String t) {
                     return Text(
                       t,
                       style: TextStyle(
@@ -108,7 +138,7 @@ class _DocumentDataState extends State<DocumentData> {
                     );
                   }).toList();
                 },
-                items: documentTypes.map((String t) {
+                items: _documentTypes.map((String t) {
                   return DropdownMenuItem<String>(
                     child: Text(
                       t,
@@ -118,36 +148,24 @@ class _DocumentDataState extends State<DocumentData> {
                   );
                 }).toList(),
                 iconEnabledColor: Colors.black54,
-              //  dropdownColor: Colors.white,
+                //  dropdownColor: Colors.white,
                 decoration: InputDecoration(
                   contentPadding:
                       EdgeInsets.only(left: 22, top: 0, right: 10, bottom: 0),
                   fillColor: Colors.white,
                   filled: true,
-                  errorStyle: TextStyle(fontSize: 0, height: 0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  errorStyle: const TextStyle(fontSize: 0, height: 0),
+                  enabledBorder: _enabledBorder,
+                  focusedBorder: _focusedBorder,
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             Container(
               margin: EdgeInsets.only(left: 22, bottom: 2),
-              child: Text(
+              child: const Text(
                 'DOCUMENT NUMBER',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -180,44 +198,20 @@ class _DocumentDataState extends State<DocumentData> {
                     color: Colors.grey[450],
                     fontWeight: FontWeight.bold,
                   ),
-                  errorStyle: TextStyle(fontSize: 0, height: 0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  errorStyle: const TextStyle(fontSize: 0, height: 0),
+                  enabledBorder: _enabledBorder,
+                  focusedBorder: _focusedBorder,
+                  errorBorder: _errorBorder,
+                  focusedErrorBorder: _focuseErrordBorder,
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             Container(
               margin: EdgeInsets.only(left: 22, bottom: 2),
-              child: Text(
+              child: const Text(
                 'ISSUE DATE',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -262,44 +256,20 @@ class _DocumentDataState extends State<DocumentData> {
                     color: Colors.grey[450],
                     fontWeight: FontWeight.bold,
                   ),
-                  errorStyle: TextStyle(fontSize: 0, height: 0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  errorStyle: const TextStyle(fontSize: 0, height: 0),
+                  enabledBorder: _enabledBorder,
+                  focusedBorder: _focusedBorder,
+                  errorBorder: _errorBorder,
+                  focusedErrorBorder: _focuseErrordBorder,
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             Container(
               margin: EdgeInsets.only(left: 22, bottom: 2),
-              child: Text(
+              child: const Text(
                 'DOCUMENT ISSUING COUNTRY',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -316,11 +286,11 @@ class _DocumentDataState extends State<DocumentData> {
                   color: Colors.grey[800],
                   fontWeight: FontWeight.bold,
                 ),
-                value: selectedCountry,
+                value: _selectedCountry,
                 onChanged: (String value) =>
-                    setState(() => selectedCountry = value),
+                    setState(() => _selectedCountry = value),
                 selectedItemBuilder: (BuildContext context) {
-                  return countries.map<Widget>((String t) {
+                  return _countries.map<Widget>((String t) {
                     return Text(
                       t,
                       style: TextStyle(
@@ -329,7 +299,7 @@ class _DocumentDataState extends State<DocumentData> {
                     );
                   }).toList();
                 },
-                items: countries.map((String t) {
+                items: _countries.map((String t) {
                   return DropdownMenuItem<String>(
                     child: Text(
                       t,
@@ -345,30 +315,18 @@ class _DocumentDataState extends State<DocumentData> {
                       EdgeInsets.only(left: 22, top: 0, right: 10, bottom: 0),
                   fillColor: Colors.white,
                   filled: true,
-                  errorStyle: TextStyle(fontSize: 0, height: 0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  errorStyle: const TextStyle(fontSize: 0, height: 0),
+                  enabledBorder: _enabledBorder,
+                  focusedBorder: _focusedBorder,
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             Container(
               margin: EdgeInsets.only(left: 22, bottom: 2),
-              child: Text(
+              child: const Text(
                 'EXPIRE DATE',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -413,44 +371,20 @@ class _DocumentDataState extends State<DocumentData> {
                     color: Colors.grey[450],
                     fontWeight: FontWeight.bold,
                   ),
-                  errorStyle: TextStyle(fontSize: 0, height: 0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  errorStyle: const TextStyle(fontSize: 0, height: 0),
+                  enabledBorder: _enabledBorder,
+                  focusedBorder: _focusedBorder,
+                  errorBorder: _errorBorder,
+                  focusedErrorBorder: _focuseErrordBorder,
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             Container(
               margin: EdgeInsets.only(left: 22, bottom: 2),
-              child: Text(
+              child: const Text(
                 'NATIONALITY',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -467,11 +401,11 @@ class _DocumentDataState extends State<DocumentData> {
                   color: Colors.grey[800],
                   fontWeight: FontWeight.bold,
                 ),
-                value: selectedNationality,
+                value: _selectedNationality,
                 onChanged: (String value) =>
-                    setState(() => selectedNationality = value),
+                    setState(() => _selectedNationality = value),
                 selectedItemBuilder: (BuildContext context) {
-                  return nationalities.map<Widget>((String t) {
+                  return _nationalities.map<Widget>((String t) {
                     return Text(
                       t,
                       style: TextStyle(
@@ -480,7 +414,7 @@ class _DocumentDataState extends State<DocumentData> {
                     );
                   }).toList();
                 },
-                items: nationalities.map((String t) {
+                items: _nationalities.map((String t) {
                   return DropdownMenuItem<String>(
                     child: Text(
                       t,
@@ -496,40 +430,28 @@ class _DocumentDataState extends State<DocumentData> {
                       EdgeInsets.only(left: 22, top: 0, right: 10, bottom: 0),
                   fillColor: Colors.white,
                   filled: true,
-                  errorStyle: TextStyle(fontSize: 0, height: 0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                  errorStyle: const TextStyle(fontSize: 0, height: 0),
+                  enabledBorder: _enabledBorder,
+                  focusedBorder: _focusedBorder,
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
             CheckboxListTile(
               controlAffinity: ListTileControlAffinity.leading,
-              title: Text(
+              title: const Text(
                 'THIS DOCUMENT IS BIOMETRIC',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Color.fromRGBO(66, 66, 66, 1),
                 ),
               ),
-              value: isBiometric,
-              onChanged: (value) => setState(() => isBiometric = value),
+              value: _isBiometric,
+              onChanged: (value) => setState(() => _isBiometric = value),
             ),
-            SizedBox(
+            const SizedBox(
               height: 18,
             ),
             Material(
@@ -548,7 +470,7 @@ class _DocumentDataState extends State<DocumentData> {
                     ? Container(
                         height: 20,
                         width: 20,
-                        child: Center(
+                        child: const Center(
                           child: CircularProgressIndicator(
                             backgroundColor: Colors.white,
                             strokeWidth: 3,
@@ -561,7 +483,7 @@ class _DocumentDataState extends State<DocumentData> {
                       ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 15,
             ),
           ],
