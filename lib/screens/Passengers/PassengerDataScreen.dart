d9530b2f964ed037ec7b8acd58aa13024d1c5154
Gimhani <gimahaniash@gmail.com>
2020-10-09 12:38:43 +0530

    Change the contact

--- lib/screens/Passengers/PassengerDataScreen.dart
+++ lib/screens/Passengers/PassengerDataScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flag/flag.dart';
+
 import 'Pas1.dart';
 import 'Pas2.dart';
 import 'Pas3.dart';
